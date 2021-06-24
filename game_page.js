742ed239d1e88f2a508b13557f6df25a5a1cb1a0
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-06-23 19:11:59 -0700

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,9 @@
+player1_name = localStorage.getItem("player1_name");
+	player2_name = localStorage.getItem("player2_name");
+
+	player1_score = 0;
+	player2_score = 0;
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
